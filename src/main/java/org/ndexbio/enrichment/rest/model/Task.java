1c6154776669a07aa324831fbc2d1951375e2e37
Chris Churas <churas.camera@gmail.com>
2019-02-21 14:11:46 -0800

    initial revision

---
+++ src/main/java/org/ndexbio/enrichment/rest/model/Task.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ndexbio.enrichment.rest.model;
+
+/**
+ *
+ * @author churas
+ */
+public class Task {
+    
+    private String _id;
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
+    
+}
