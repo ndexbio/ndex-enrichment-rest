90e649249e8fff0f76a1ae7ccee7fdadc2c48901
Chris Churas <churas.camera@gmail.com>
2020-02-04 10:37:36 -0800

    moved logic to build database into factory

--- src/main/java/org/ndexbio/enrichment/rest/engine/BasicEnrichmentEngineFactory.java
+++ src/main/java/org/ndexbio/enrichment/rest/engine/BasicEnrichmentEngineFactory.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.ndexbio.enrichment.rest.engine;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.ndexbio.ndexsearch.rest.model.DatabaseResult;
 import org.ndexbio.enrichment.rest.model.InternalDatabaseResults;
@@ -26,43 +23,58 @@ public class BasicEnrichmentEngineFactory {
     
     static Logger _logger = LoggerFactory.getLogger(BasicEnrichmentEngineFactory.class);
 
-    private String _dbDir;
-    private String _taskDir;
-    private InternalDatabaseResults _databaseResults;
-    private int _numWorkers;
+    private final String _dbDir;
+    private final String _taskDir;
+    private final InternalDatabaseResults _databaseResults;
+	private final int _numWorkers;
+	private ExecutorServiceFactory _executorServiceFac;
     
     /**
      * Temp directory where query results will temporarily be stored.
      * @param tmpDir 
      */
-    public BasicEnrichmentEngineFactory(Configuration config){
+    public BasicEnrichmentEngineFactory(Configuration config) throws NullPointerException {
         
         _dbDir = config.getEnrichmentDatabaseDirectory();
         _taskDir = config.getEnrichmentTaskDirectory();
         _databaseResults = config.getNDExDatabases();
-        _numWorkers = config.getNumberWorkers();
+		if (_databaseResults == null){
+			throw new NullPointerException("Unable to retrieve database");
+		}
+		_numWorkers = config.getNumberWorkers();
+		_executorServiceFac = new ExecutorServiceFactoryImpl();
     }
     
-    
+    protected void setAlternateExecutorServiceFactory(ExecutorServiceFactory esf){
+		_executorServiceFac = esf;
+	}
+
     /**
      * Creates EnrichmentEngine
      * @return 
      */
     public EnrichmentEngine getEnrichmentEngine() throws EnrichmentException {
-    	_logger.debug("Creating executor service with: " + Integer.toString(_numWorkers) + " workers");
-    	ExecutorService es = Executors.newFixedThreadPool(_numWorkers);
-    	
-        BasicEnrichmentEngineImpl enricher = new BasicEnrichmentEngineImpl(es, _dbDir, _taskDir);
-        enricher.setDatabaseResults(_databaseResults);
-        for (DatabaseResult dr : _databaseResults.getResults()){
-            _logger.debug("Loading: " + dr.getName());
-            addGeneMapToEnricher(enricher, dr);
-            _logger.debug("Done with loading");
-        }
+		_logger.debug("Creating executor service with: " + Integer.toString(_numWorkers) + " workers");
+    	ExecutorService es = _executorServiceFac.getExecutorService(_numWorkers);
+    	ConcurrentHashMap<String, ConcurrentHashMap<String, HashSet<String>>> databases = new ConcurrentHashMap<>();
+        
+		if (_databaseResults.getResults() == null){
+			throw new NullPointerException("No data found in database");
+		}
+		for (DatabaseResult dr : _databaseResults.getResults()){
+			_logger.debug("Loading: " + dr.getName());
+			addGeneMapToEnricher(databases, dr);
+			_logger.debug("Done with loading");
+		}
+
+		BasicEnrichmentEngineImpl enricher = new BasicEnrichmentEngineImpl(es, _dbDir, _taskDir);
+		enricher.setDatabaseResults(_databaseResults);
+		enricher.setDatabaseMap(databases);
         return enricher;
     }
     
-    protected void addGeneMapToEnricher(BasicEnrichmentEngineImpl enricher,
+	
+    private void addGeneMapToEnricher(ConcurrentHashMap<String, ConcurrentHashMap<String, HashSet<String>>> databases,
             DatabaseResult dr) throws EnrichmentException{
         for (InternalGeneMap igm : _databaseResults.getGeneMapList()){
             if (igm.getDatabaseUUID().equals(dr.getUuid())){
@@ -74,9 +86,40 @@ public class BasicEnrichmentEngineFactory {
                     continue;
                 } 
                 for (String gene : igm.getGeneMap().keySet()){
-                    enricher.addGeneToDatabase(dr.getUuid(), gene, igm.getGeneMap().get(gene));
+                    addGeneToDatabase(databases, dr.getUuid(), gene, igm.getGeneMap().get(gene));
                 }
             }
         }
-    }    
+    }
+	
+	/**
+	 * Adds {@code gene} gene symbol (as upper case) to HashMap specified by 
+	 * {@code databaseId} to allow quick retrieval of all networks for a given
+	 * database that contain a specific gene.
+	 * 
+	 * Structure of data being stored.
+	 * 
+	 * {@code databaseId} -> {@code gene upper case} -> {@code networkIds}
+	 * 
+	 * @param databaseId Unique identifier for the database
+	 * @param gene Gene name
+	 * @param networkIds {@link java.util.Collection} of network ids
+	 */
+	private void addGeneToDatabase(ConcurrentHashMap<String, ConcurrentHashMap<String, HashSet<String>>> databases, final String databaseId, final String gene,
+			Collection<String> networkIds){
+		
+		String geneUpperCase = gene.toUpperCase();
+		ConcurrentHashMap<String, HashSet<String>> dbHash = databases.get(databaseId);
+		if (dbHash == null){
+			dbHash = new ConcurrentHashMap<>();
+			databases.put(databaseId, dbHash);
+		}
+		HashSet<String> geneSet = dbHash.get(geneUpperCase);
+		if (geneSet == null){
+			geneSet = new HashSet<>();
+			dbHash.put(geneUpperCase, geneSet);
+		}
+		geneSet.clear();
+		geneSet.addAll(networkIds);
+	}
 }
