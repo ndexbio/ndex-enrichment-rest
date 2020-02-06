495160c94916b0d16741a3e68140fb8d3cda33e7
Chris Churas <churas.camera@gmail.com>
2020-02-05 17:00:47 -0800

    added lots of tests

--- src/test/java/org/ndexbio/enrichment/rest/engine/util/TestBasicEnrichmentEngineRunner.java
+++ src/test/java/org/ndexbio/enrichment/rest/engine/util/TestBasicEnrichmentEngineRunner.java
@@ -1,7 +1,13 @@
 package org.ndexbio.enrichment.rest.engine.util;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.ndexbio.enrichment.rest.model.EnrichmentQueryResult;
+import org.ndexbio.enrichment.rest.model.EnrichmentQueryResults;
 /**
  *
  * @author churas
@@ -9,10 +15,82 @@ import static org.junit.Assert.*;
 public class TestBasicEnrichmentEngineRunner {
 	
 	@Test
-	public void testFoo(){
-		assertEquals(1, 1);
+	public void testGetPvalue(){
+		BasicEnrichmentEngineRunner runner = new BasicEnrichmentEngineRunner(null,null,null,
+				null,null,null,null,null);
+		
+		// test where total genes in universe is negative
+		assertEquals(Double.MAX_VALUE, runner.getPvalue(-5, 100, 10, 5), 0.001);
+		
+		// test where total genes in network is negative
+		assertEquals(Double.MAX_VALUE, runner.getPvalue(1000, -3, 10, 5), 0.001);
+		
+		// test where query size greater then universe
+		assertEquals(Double.MAX_VALUE, runner.getPvalue(50, 20, 100, 5), 0.001);
 	}
 	
+	@Test
+	public void testGetUniqueQueryGeneSetFilteredByUniverseGenes(){
+		HashSet<String> uniqueGeneSet = new HashSet<>();
+		BasicEnrichmentEngineRunner runner = new BasicEnrichmentEngineRunner(null,null,null,
+				null,null,uniqueGeneSet,null,null);
+		
+		// try where universe is empty and input is null
+		assertEquals(0, runner.getUniqueQueryGeneSetFilteredByUniverseGenes(null).size());
+		
+		TreeSet<String> genes = new TreeSet<>();
+		genes.add("gene1");
+		// try where universe is empty and input is one gene
+		assertEquals(0, runner.getUniqueQueryGeneSetFilteredByUniverseGenes(genes).size());
+		
+		// try where universe has some genes
+		uniqueGeneSet.add("gene1");
+		uniqueGeneSet.add("Gene2");
+		uniqueGeneSet.add("gene3");
+		runner = new BasicEnrichmentEngineRunner(null,null,null,
+				null,null,uniqueGeneSet,null,null);
+		
+		SortedSet<String> res = runner.getUniqueQueryGeneSetFilteredByUniverseGenes(genes);
+		assertEquals(1, res.size());
+		assertTrue(res.contains("gene1"));
+		
+		genes.add("Gene3");
+		res = runner.getUniqueQueryGeneSetFilteredByUniverseGenes(genes);
+		assertEquals(1, res.size());
+		assertTrue(res.contains("gene1"));
+		
+		genes.add("gene3");
+		res = runner.getUniqueQueryGeneSetFilteredByUniverseGenes(genes);
+		assertEquals(2, res.size());
+		assertTrue(res.contains("gene1"));
+		assertTrue(res.contains("gene3"));
+		
+	}
+	
+	@Test
+	public void testUpdateEnrichmentQueryResults(){
+		EnrichmentQueryResults eqr = new EnrichmentQueryResults();
+		eqr.setStartTime(100);
+		BasicEnrichmentEngineRunner runner = new BasicEnrichmentEngineRunner(null,null,null,
+				null,null,null,null,eqr);
+		
+		// test update with null result
+		runner.updateEnrichmentQueryResults("done", 55, null);
+		assertEquals(55, eqr.getProgress());
+		assertEquals("done", eqr.getStatus());
+		assertTrue(eqr.getWallTime() > 0);
+		
+		// test with a few results
+		ArrayList<EnrichmentQueryResult> resList = new ArrayList<>();
+		resList.add(new EnrichmentQueryResult());
+		resList.add(new EnrichmentQueryResult());
+		runner.updateEnrichmentQueryResults("ha", 85, resList);
+		assertEquals(85, eqr.getProgress());
+		assertEquals("ha", eqr.getStatus());
+		assertTrue(eqr.getWallTime() > 0);
+		assertEquals(2, eqr.getNumberOfHits());
+		assertNotNull(eqr.getResults());
+	}
 	/**
     @Test
     public void testprocessQuery() throws Exception{
